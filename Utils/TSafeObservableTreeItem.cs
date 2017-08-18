0e6e813568477ce4b213d116fa69b19fec99f790
Kevin Mackenzie <dev.kjmack305@gmail.com>
2017-08-18 00:53:54 -0400

    Begain work on BlackList ViewModel
    This will need to include a tree-type data structure that supports
    binding (mostly for ease of use,not strictly required for the data
    loaded itself).

---
+++ Utils/TSafeObservableTreeItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalCloudStorage
+{
+    /// <summary>
+    /// Represents a leaf on a tree of items
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    public class TSafeObservableTreeItem<TValue> : ViewModelBase
+    {
+        private TValue _value;
+        private TSafeObservableCollection<TSafeObservableTreeItem<TValue>> _children;
+
+        public TSafeObservableTreeItem()
+        {
+        }
+
+        #region Public Properties
+        public TSafeObservableCollection<TSafeObservableTreeItem<TValue>> Children
+        {
+            get
+            {
+                if(_children == null)
+                    _children = new TSafeObservableCollection<TSafeObservableTreeItem<TValue>>();
+                return _children;
+            }
+        }
+        public TValue Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+    }
+}
---
+++ ViewModel/BlackListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalCloudStorage.ViewModel
+{
+    public class BlackListViewModel : ViewModelBase
+    {
+        private TSafeObservableCollection<>
+        public BlackListViewModel(IEnumerable<string> blackList)
+        {
+            BlackList = blackList;
+        }
+
+        public ReadOnlyObservableCollection<string> BlackList { get; }
+
+
+        public void Add
+    }
+}
